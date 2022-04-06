64925af9a7978864ba5c770659cefdae53a446d0
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-04-06 15:37:49 +0100

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,9 @@
 import React, { Component } from 'react';
-import calculate from './logic/calculate'
-import './Calculator.css';
 import { v4 as uuidv4 } from 'uuid';
+import calculate from './logic/calculate';
+import './Calculator.css';
 
 class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        next: '',
-        total: '',
-        operation: ''
-    };
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick (btn) {
-    console.log("btn",btn);
-    this.setState(state => 
-     calculate(
-      {
-        next: state.next,
-        total: state.total,
-        operation: state.operation
-      } 
-      , btn)
-
-    )
-  }
-  
   btnValues = [
     ['C', '+-', '%', '/'],
     ['7', '8', '9', 'x'],
@@ -36,7 +12,26 @@ class Calculator extends Component {
     ['0', '.', '='],
   ];
 
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: '',
+      total: '',
+      operation: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(btn) {
+    this.setState((state) => calculate(
+      {
+        next: state.next,
+        total: state.total,
+        operation: state.operation,
+      },
+      btn,
+    ));
+  }
 
   btnClass = (param) => {
     switch (param) {
@@ -56,25 +51,24 @@ class Calculator extends Component {
   }
 
   render() {
+    const { next, operation, total } = this.state;
     return (
       <div className="wrapper">
-        <div className="screen" >
-          {this.state.next || this.state.operation || this.state.total || 0}
+        <div className="screen">
+          {next || operation || total || 0}
         </div>
         <div className="btnBox">
 
           {/* Display buttons */}
           {this.btnValues.flat().map((btn) => (
-            
+
             <button
               type="submit"
               key={uuidv4()}
               className={this.btnClass(btn)}
-              onClick={() =>
-                {
-                  this.handleClick(btn)
-              }
-              }
+              onClick={() => {
+                this.handleClick(btn);
+              }}
             >
               {btn}
             </button>
